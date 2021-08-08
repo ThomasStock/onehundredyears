ae74faa2f28110c6c378828571d16baa9c9ad7bc
thomas stock <thomasstock1985@gmail.com>
2021-08-08 18:27:21 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p style={{ fontSize: 54 }}>Jaar 0 ...</p>
+			</header>
+		</div>
+	)
+}
+
+export default App
